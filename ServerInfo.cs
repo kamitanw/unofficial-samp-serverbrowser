2c2cc012eb521e50b72884b851fec910ec880efe
yugecin <yugecin@users.noreply.github.com>
2016-10-26 22:42:51 +0200

    base serverquery

---
+++ ServerInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace yugecin.sampbrowser
+{
+	struct ServerInfo
+	{
+		public bool online;
+		public bool password;
+		public IPEndPoint address;
+		public string ip;
+		public int port;
+		public string hostname;
+		public int players;
+		public int maxplayers;
+		public int ping;
+		public string mode;
+		public string language;
+	}
+}
---
+++ ServerQuery.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace yugecin.sampbrowser
+{
+	class ServerQuery
+	{
+
+		public ServerInfo GetDefaultInfo( ServerInfo info )
+		{
+			IPAddress ipaddress;
+			if( !IPAddress.TryParse( info.ip, out ipaddress ) )
+			{
+				return info;
+			}
+			UdpClient c = openConnection( ipaddress, info );
+			if( info.online )
+			{
+
+			}
+			c.Close();
+			return info;
+		}
+
+		public UdpClient openConnection( IPAddress ipaddress, ServerInfo info )
+		{
+			byte[] ipbytes = ipaddress.GetAddressBytes();
+			IPEndPoint IPEP = new IPEndPoint( ipaddress, info.port );
+			UdpClient c = new UdpClient();
+			c.Client.SendTimeout = 3000;
+			c.Client.ReceiveTimeout = 3000;
+			c.Connect( IPEP );
+			c.Send( new byte[] {
+				(byte) 'S', (byte) 'A', (byte) 'M', (byte) 'P',
+				ipbytes[0], ipbytes[1], ipbytes[2], ipbytes[3],
+				(byte) ( info.port & 0xFF ), (byte) ( ( info.port >> 8 ) & 0xFF )
+			}, 15 );
+			byte[] response = c.Receive( ref IPEP );
+			if( response.Length >= 15 )
+			{
+				info.online = (
+					response[10] == (byte) 'p' &&
+					response[11] == (byte) '4' &&
+					response[12] == (byte) '1' &&
+					response[13] == (byte) '5' &&
+					response[14] == (byte) '0'
+				);
+			}
+			return c;
+		}
+
+	}
+}
