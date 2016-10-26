e8654e8df8f0f8bba4f16a37868310337570b344
yugecin <yugecin@users.noreply.github.com>
2016-10-26 22:11:29 +0200

    serverprovider concept

---
+++ serverproviders/IServerProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace yugecin.sampbrowser
+{
+	interface IServerProvider
+	{
+
+		IPEndPoint GetNext();
+
+	}
+}
---
+++ serverproviders/TestServerProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace yugecin.sampbrowser
+{
+	class TestServerProvider : IServerProvider
+	{
+
+		public IPEndPoint GetNext() {
+			return new IPEndPoint( new IPAddress( new byte[] { 127, 0, 0, 1 } ), 7777 );
+		}
+
+	}
+}
