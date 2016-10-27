1c3e047b0b8de3f7e16a517998ba1cb7f9b4168d
yugecin <yugecin@users.noreply.github.com>
2016-10-27 22:56:15 +0200

    dont use struct for serverinfo

--- ServerInfo.cs
+++ ServerInfo.cs
@@ -2,10 +2,12 @@
 
 namespace yugecin.sampbrowser
 {
-	struct ServerInfo
+	class ServerInfo
 	{
+
 		public bool online;
 		public bool password;
+		public byte[] ipbytes;
 		public IPEndPoint address;
 		public string ip;
 		public int port;
@@ -15,5 +17,18 @@ namespace yugecin.sampbrowser
 		public int ping;
 		public string mode;
 		public string language;
+
+		public string[] GetListItemText()
+		{
+			return new string[] {
+				password ? "X" : "",
+				hostname,
+				players + "/" + maxplayers,
+				ping.ToString(),
+				mode,
+				language
+			};
+		}
+
 	}
 }
