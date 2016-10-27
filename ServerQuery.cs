c2aed42738d13b25470275615d193d33f5cdc0c9
yugecin <yugecin@users.noreply.github.com>
2016-10-27 22:56:39 +0200

    make serverquery work better

--- ServerQuery.cs
+++ ServerQuery.cs
@@ -1,56 +1,76 @@
-﻿using System.Net;
+﻿using System;
+using System.Diagnostics;
+using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace yugecin.sampbrowser
 {
 	class ServerQuery
 	{
 
-		public ServerInfo GetDefaultInfo( ServerInfo info )
+		private Stopwatch pingTimer;
+
+		public ServerQuery()
+		{
+			pingTimer = new Stopwatch();
+		}
+
+		public void LoadInitial( ServerInfo info )
 		{
 			IPAddress ipaddress;
 			if( !IPAddress.TryParse( info.ip, out ipaddress ) )
 			{
-				return info;
+				return;
 			}
-			UdpClient c = openConnection( ipaddress, info );
-			if( info.online )
-			{
-
-			}
-			c.Close();
-			return info;
+			info.ipbytes = ipaddress.GetAddressBytes();
+			info.address = new IPEndPoint( ipaddress, info.port );
+			GetDefaultInfo( info );
 		}
 
-		public UdpClient openConnection( IPAddress ipaddress, ServerInfo info )
+		private void GetDefaultInfo( ServerInfo info )
 		{
-			byte[] ipbytes = ipaddress.GetAddressBytes();
-			IPEndPoint IPEP = new IPEndPoint( ipaddress, info.port );
+			byte[] response;
+			IPEndPoint IPEP = new IPEndPoint( info.address.Address, info.port );
 			UdpClient c = new UdpClient();
 			c.Client.SendTimeout = 3000;
 			c.Client.ReceiveTimeout = 3000;
 			c.Connect( IPEP );
-			c.Send( new byte[] {
-				(byte) 'S', (byte) 'A', (byte) 'M', (byte) 'P',
-				ipbytes[0], ipbytes[1], ipbytes[2], ipbytes[3],
-				(byte) ( info.port & 0xFF ), (byte) ( ( info.port >> 8 ) & 0xFF ),
-				0x70, 0x34, 0x31, 0x35, 0x0
-			}, 15 );
-			System.Console.WriteLine( "sent");
-			byte[] response = c.Receive( ref IPEP );
-			System.Console.WriteLine( "hiiii" );
-			System.Console.WriteLine( response.Length );
-			if( response.Length >= 15 )
+			pingTimer.Start();
+			try
 			{
-				info.online = (
-					response[10] == (byte) 'p' &&
-					response[11] == (byte) '4' &&
-					response[12] == (byte) '1' &&
-					response[13] == (byte) '5' &&
-					response[14] == (byte) '0'
-				);
+				c.Send( new byte[] {
+					(byte) 'S', (byte) 'A', (byte) 'M', (byte) 'P',
+					info.ipbytes[0], info.ipbytes[1], info.ipbytes[2], info.ipbytes[3],
+					(byte) ( info.port & 0xFF ), (byte) ( ( info.port >> 8 ) & 0xFF ),
+					0x69
+				}, 11 );
+				response = c.Receive( ref IPEP );
+				c.Close();
 			}
-			return c;
+			catch( Exception )
+			{
+				return;
+			}
+			info.ping = (int) pingTimer.ElapsedMilliseconds;
+			pingTimer.Reset();
+			if( response.Length < 28 )
+			{
+				return;
+			}
+			info.online = (
+				response[0] == (byte) 'S' &&
+				response[1] == (byte) 'A' &&
+				response[2] == (byte) 'M' &&
+				response[3] == (byte) 'P'
+			);
+			if( !info.online )
+			{
+				return;
+			}
+			info.password = response[10] == 0x1;
+			info.players = response[11] << 8 | response[12];
+			info.maxplayers = response[13] << 8 | response[14];
 		}
 
 	}
