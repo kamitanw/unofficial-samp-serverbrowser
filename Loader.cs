f39b6a88c275a89abee4d4186ecea58f011baf35
yugecin <yugecin@users.noreply.github.com>
2016-10-29 14:06:24 +0200

    test the launcher

--- Loader.cs
+++ Loader.cs
@@ -5,10 +5,21 @@ namespace yugecin.sampbrowser
 	class Loader
 	{
 
-		static void Main()
+		public static void Main()
 		{
 			Application.VisualStyleState = System.Windows.Forms.VisualStyles.VisualStyleState.NoneEnabled;
+#if DEBUGBUILD
 			Application.Run( new frmMain( new TestServerProvider() ) );
+#else
+			Application.Run( new frmMain( new GamestateServerProvider() ) );
+#endif
+#if WITHLAUNCHER
+			MLauncher launcher = new MLauncher();
+			launcher.Launch();
+#if DEBUGBUILD
+			System.Console.WriteLine( "with launcher" );
+#endif
+#endif
 		}
 
 	}
