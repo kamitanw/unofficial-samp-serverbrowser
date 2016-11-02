56e60e9edb73a7f7fd8e5138c9e1b24a077b7548
yugecin <yugecin@users.noreply.github.com>
2016-11-02 11:28:46 +0100

    get hostname/mode/lang from query

--- ServerQuery.cs
+++ ServerQuery.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Threading;
 
 namespace yugecin.sampbrowser
@@ -68,9 +69,34 @@ namespace yugecin.sampbrowser
 			{
 				return;
 			}
-			info.password = response[10] == 0x1;
-			info.players = response[11] << 8 | response[12];
-			info.maxplayers = response[13] << 8 | response[14];
+			info.password = response[11] == 0x1;
+			info.players = response[12] | response[13] << 8;
+			info.maxplayers = response[14] | response[15] << 8;
+			int position = 16;
+			position = ReadString( response, position, 4, out info.hostname );
+			position = ReadString( response, position, 4, out info.mode );
+			position = ReadString( response, position, 4, out info.language );
+		}
+
+		private int ReadString( byte[] data, int position, int lengthsize, out string result )
+		{
+			int len = 0;
+			for( int i = 0; i < lengthsize; i++ )
+			{
+				len |= data[position++] << ( i * 8 );
+			}
+			if( len == 0 )
+			{
+				result = "-";
+				return position;
+			}
+			byte[] str = new byte[len];
+			for( int i = 0; i < len; i++ )
+			{
+				str[i] = data[position++];
+			}
+			result = Encoding.GetEncoding( 1252 ).GetString( str );
+			return position;
 		}
 
 	}
