74c2974645e88514f486825fa952ab00d60a45e0
yugecin <yugecin@users.noreply.github.com>
2016-11-05 17:25:21 +0100

    update/optimize buildscript

--- build.bat
+++ build.bat
@@ -19,19 +19,22 @@ call %p%
 echo cpp
 cl.exe /Zi /MD /c ulauncher.cpp
 cl.exe /Zi /clr /c mlauncher.cpp
-csc /target:module ServerInfo.cs
+echo.
+echo structs
+csc /target:module %~2 ServerInfo.cs
+csc /target:module %~2 /addmodule:ServerInfo.netmodule ServerRow.cs
 echo serverquery
 csc /target:module %~2 /addmodule:ServerInfo.netmodule ServerQuery.cs
 echo serverproviders
 csc /target:module %~2 /addmodule:ServerInfo.netmodule serverproviders/IServerProvider.cs
-csc /target:module %~2 /addmodule:ServerInfo.netmodule /addmodule:IserverProvider.netmodule serverproviders/TestServerProvider.cs
-csc /target:module %~2 /addmodule:ServerInfo.netmodule /addmodule:IserverProvider.netmodule serverproviders/GameStateServerProvider.cs
+csc /target:module %~2 /addmodule:ServerInfo.netmodule /addmodule:IServerProvider.netmodule serverproviders/TestServerProvider.cs
+csc /target:module %~2 /addmodule:ServerInfo.netmodule /addmodule:IServerProvider.netmodule serverproviders/GameStateServerProvider.cs
 echo frmmain
-csc /target:module %~2 /addmodule:ServerInfo.netmodule /addmodule:IServerProvider.netmodule /addmodule:TestServerProvider.netmodule /addmodule:ServerQuery.netmodule FrmMain.cs FrmMain.Designer.cs
+csc /target:module %~2 /addmodule:ServerInfo.netmodule /addmodule:ServerRow.netmodule /addmodule:IServerProvider.netmodule /addmodule:ServerQuery.netmodule FrmMain.cs FrmMain.Designer.cs
 echo loader
 csc /target:module %~2 /addmodule:FrmMain.netmodule /addmodule:IServerProvider.netmodule /addmodule:TestServerProvider.netmodule /addmodule:GameStateServerProvider.netmodule /addmodule:mlauncher.obj /define:WITHLAUNCHER Loader.cs
 echo.
 echo - linking
-link Advapi32.lib user32.lib mlauncher.obj ulauncher.obj ServerInfo.netmodule IServerProvider.netmodule TestServerProvider.netmodule GameStateServerProvider.netmodule ServerQuery.netmodule frmMain.netmodule Loader.netmodule /entry:yugecin.sampbrowser.Loader.Main /subsystem:%subsystem% /out:sampbrowser.exe /ltcg
+link Advapi32.lib user32.lib mlauncher.obj ulauncher.obj ServerInfo.netmodule ServerRow.netmodule IServerProvider.netmodule TestServerProvider.netmodule GameStateServerProvider.netmodule ServerQuery.netmodule frmMain.netmodule Loader.netmodule /entry:yugecin.sampbrowser.Loader.Main /subsystem:%subsystem% /out:sampbrowser.exe /ltcg
 
 pause
